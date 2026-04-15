d1350b54d052c4c9c8564835a1adc05aaae63b7e
arnav2ndmail-collab <arnav2ndmail@gmail.com>
2026-04-15 16:25:57 +0530

    Update to TestZyro v7

--- open-next.config.ts
+++ open-next.config.ts
@@ -8,5 +8,17 @@ incrementalCache: "dummy",
 tagCache: "dummy",
 queue: "direct"
 }
+},
+edgeExternals: ["node:crypto"],
+middleware: {
+external: true,
+override: {
+wrapper: "cloudflare-edge",
+converter: "edge",
+proxyExternalRequest: "fetch",
+incrementalCache: "dummy",
+tagCache: "dummy",
+queue: "direct"
+}
 }
 };
